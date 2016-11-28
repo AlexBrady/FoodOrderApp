5c3843e5c7b667d7e42923679c2efe73ad7caaad
FriskyButcher <a-brady-9@hotmail.com>
2016-11-28 17:51:17 +0000

    Pay added, no function yet

---
+++ app/src/main/java/com/example/friskybutcher/foodorder/Pay.java
@@ -0,0 +1,15 @@
+package com.example.friskybutcher.foodorder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pay extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay);
+    }
+}
